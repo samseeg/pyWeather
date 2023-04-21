213d7ebf1b510aa6f91e6ea9c9e662abc4d56ea1
sambseeg <samseegmiller@gmail.com>
2023-04-21 10:47:20 -0700

    first go around of trying to get a basic python weather app going

---
+++ weather.py
@@ -0,0 +1,53 @@
+import requests
+import datetime
+
+# Function to get location coordinates
+
+
+def get_location_coords(location):
+    url = f"https://geocoding-api.open-meteo.com/v1/search?name={location}&count=1&language=en&format=json"
+    response = requests.get(url).json()
+    if len(response) == 0:
+        return None
+    else:
+        # print('location response', response["results"][0])
+        res = response["results"][0]
+        coords = {
+            "latitude": res["latitude"],
+            "longitude": res["longitude"],
+            "timezone": res["timezone"]
+        }
+        return coords
+
+# Function to get weather forecast data
+
+
+def get_weather_forecast(coords):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={coords['latitude']}&longitude={coords['longitude']}&hourly=temperature_2m,precipitation_probability,precipitation&current_weather=true&timezone={coords['timezone']}"
+    response = requests.get(url).json()
+    # print("get weather data", response)
+    return response
+
+# Function to print weather forecast
+
+
+def print_weather_forecast(location, data):
+    # print(f"Weather forecast for {location, data}:")
+    for forecast in data["daily"]:
+        date = datetime.datetime.fromtimestamp(
+            forecast["timestamp"]).strftime("%A, %B %d")
+        temp = round(forecast["temperature_2m"]["max"], 1)
+        rain = round(forecast["precipitation"]["value"], 1)
+        weather = forecast["weathercode"]["value"]
+        print(f"{date}: {temp}°C, {rain} mm rain, {weather}")
+
+
+# Main program
+location = input("Enter location (city, state/province, country): ")
+coords = get_location_coords(location)
+if coords is None:
+    print(f"{location} not found.")
+else:
+    print('coords', coords)
+    data = get_weather_forecast(coords)
+    print_weather_forecast(location, data)
